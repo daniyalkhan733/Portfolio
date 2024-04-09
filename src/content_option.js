8a3e949e6a8e734670ecebd9e88067017835f5e7
daniyal-p <daniyal733khan@gmail.com>
2024-04-09 12:55:24 +0530

    testing final

---
+++ src/content_option.js
@@ -0,0 +1,166 @@
+const logotext = "Daniyal";
+const meta = {
+    title: "Daniyal Khan",
+    description: "I’m Daniyal Khan (MERN)-Full stack devloper,currently working on Myself",
+};
+
+const introdata = {
+    title: "I’m Daniyal Khan",
+    animated: {
+        first: "I love Coding",
+        second: "I Build Websites",
+        third: "I develop mobile apps",
+    },
+    description: "Expert React developer crafting seamless, scalable, and stunning web experiences.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "All about my self",
+    aboutme: "With a fervent passion for web development spanning five years, I've honed my skills in crafting dynamic digital experiences. From managing educational websites to contributing to impactful initiatives like the Edhi Foundation, my journey reflects a commitment to excellence and innovation. Currently delving into React, I aim to push boundaries and elevate web development standards.",
+};
+const worktimeline = [{
+        jobtitle: "Website Manager",
+        where: "SS CLASSES",
+        date: "(May 2022 - October 2022)",
+    },
+    {
+        jobtitle: "Wordpress Developer",
+        where: "Techno Serve Solutions",
+        date: "(October 2023 - December 2023)",
+    },
+    {
+        jobtitle: "React Developer",
+        where: "Techno Serve Solutions",
+        date: "(December 2023 - Present)",
+    },
+];
+
+const skills = [{
+        name: "React",
+        value: 90,
+    },
+    {
+        name: "Tailwind,Bootsrap",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 75,
+    },
+    {
+        name: "Wordpress",
+        value: 60,
+    },
+    {
+        name: "Astro",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Proficient in Figma, Webflow, and Flutter Flow, merging design expertise with tech finesse to create captivating digital experiences.",
+    },
+    {
+        title: "React Websites",
+        description: "Crafted two React-based charity websites for UK-based organizations, seamlessly blending innovation with empathy to amplify their philanthropic impact.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "I specialize in WordPress, crafting three travel agency websites with e-commerce, seamlessly blending adventure with exceptional user experiences.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "daniyal733khan@gmail.com",
+    YOUR_FONE: "+91 7738875007",
+    description: "Mumbai, Maharashtra",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_sqal1kf",
+    YOUR_TEMPLATE_ID: "template_yp3eg4n",
+    YOUR_USER_ID: "fLlb0BT1qK0TyGsJL",
+};
+
+const socialprofils = {
+    github: "https://github.com/daniyalkhan733",
+    instagram: "https://www.instagram.com/daniyal_773/",
+    linkedin: "https://www.linkedin.com/in/daniyal-khan-3bb6b0220/",
+    // twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
